9f132f7b6b71f1b182b86a430ffaf037adad0d60
crescentpartha <crescentpartha406@gmail.com>
2022-06-21 14:06:08 +0600

    Object Basics in JS

---
+++ stack-learner/chapter-07/01object.js
@@ -0,0 +1,29 @@
+
+// 50. Objects in JavaScript
+
+/* 
+    Objects Basics:
+        1. In Js, almost "Everything" is an Object (like --> Booleans, Numbers, Strings, Dates, Maths, Regular Expressions, Arrays, Functions, Objects)
+        2. All JavaScript values, except primitives, are objects.
+        3. Primitive values are immutable (hardcoded & can't be changed)
+        4. Object are variables & contain many values.
+        5. Object values are written as name:value pairs
+        6. Object is a collection of named values
+        7. In JS, the named values are called properties.
+        8. Object properties can be primitive values, other objects, and functions.
+*/
+
+var person = {
+    firstName: "John", 
+    lastName: "Doe", 
+    age: 50, 
+    eyeColor: "blue"
+};
+
+console.log(person) // { firstName: 'John', lastName: 'Doe', age: 50, eyeColor: 'blue' }
+console.log(person.firstName) // John
+
+
+
+
+
